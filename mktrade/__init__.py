28a9166457cbcad57e3a515f86cf1fb44749b0d7
zzzzlzzzz@yandex.by <zzzzlzzzz@yandex.by>
2019-03-23 12:19:40 +0400

    Adding password-input functional

--- mktrade/__init__.py
+++ mktrade/__init__.py
@@ -1,6 +1,8 @@
-from os import environ, path, makedirs
+from os import environ, path, makedirs, urandom
+from hashlib import sha1
 from contextlib import suppress
-from flask import Flask
+from flask import Flask, request, session, abort
+from werkzeug.security import safe_str_cmp
 from .database import db
 from . import keystore
 
@@ -15,7 +17,9 @@ def create_application() -> Flask:
     db_file = path.join(app.instance_path, 'mktrade.sqlite')
     app.config.from_mapping(SECRET_KEY='dev',
                             SQLALCHEMY_DATABASE_URI=f'sqlite:///{db_file}',
-                            SQLALCHEMY_TRACK_MODIFICATIONS=False)
+                            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+                            CSRF_FORM_NAME='csrf_token',
+                            CSRF_SESSION_NAME='csrf_token')
     if environ.get('DEBUG', 0):
         app.config.from_mapping(ENV='development', DEBUG=True)
 
@@ -30,4 +34,19 @@ def create_application() -> Flask:
     app.register_blueprint(keystore.bp)
     app.add_url_rule('/', endpoint='keystore.auth')
 
+    @app.context_processor
+    def csrf_protection_generator() -> dict:
+        if app.config['CSRF_SESSION_NAME'] not in session:
+            session[app.config['CSRF_SESSION_NAME']] = sha1(urandom(64)).hexdigest()
+        return {app.config['CSRF_FORM_NAME']: session[app.config['CSRF_SESSION_NAME']]}
+
+    @app.before_request
+    def csrf_protection_validator() -> None:
+        if request.method == 'POST':
+            token = session.get(app.config['CSRF_SESSION_NAME'], None)
+            if not token or not safe_str_cmp(token, request.form.get(app.config['CSRF_FORM_NAME'], '')):
+                abort(403)
+
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
     return app
--- mktrade/keystore.py
+++ mktrade/keystore.py
@@ -1,7 +1,5 @@
-from flask import Blueprint, request
-from flask_wtf import FlaskForm
-from wtforms import PasswordField
-from wtforms.validators import Length
+from functools import wraps
+from flask import Blueprint, request, render_template, session, url_for, redirect, flash
 
 
 bp = Blueprint('keystore', __name__, url_prefix='/keystore')
@@ -14,9 +12,28 @@ def auth():
 
     :return: Response
     """
-    class AuthForm(FlaskForm):
-        password = PasswordField('password', [Length(10)])
-
     if request.method == 'POST':
-        pass
-    return 'Work'
+        password = request.form.get('pass', None)
+        error = None
+        if not password or len(password) < 10:
+            error = 'Password length less that 10 symbols'
+        if not error:
+            session['password'] = password
+            return redirect(url_for('keystore.keys'))
+        flash(error)
+    return render_template('keystore/auth.html')
+
+
+def password_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'password' not in session:
+            return redirect(url_for('keystore.auth'))
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@bp.route('/keys', methods=('GET', 'POST'))
+@password_required
+def keys():
+    return 'keys'
