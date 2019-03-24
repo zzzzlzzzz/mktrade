f520b37c714d19ac518a95a7911b16281c912836
zzzzlzzzz@yandex.by <zzzzlzzzz@yandex.by>
2019-03-24 18:16:14 +0400

    Adding account specific functional

--- mktrade/database.py
+++ mktrade/database.py
@@ -8,6 +8,7 @@ class Account(db.Model):
     __tablename__ = 'account'
 
     account_id = db.Column(db.Integer, primary_key=True)
+    account_tag = db.Column(db.String(64), nullable=False)
     exchange_id = db.Column(db.String(64), nullable=False)
     api_key = db.Column(db.String(256), nullable=False)
     api_secret = db.Column(db.String(256), nullable=False)
@@ -16,3 +17,13 @@ class Account(db.Model):
     exchange_timeout = db.Column(db.Integer(), default=10000)
     nonce_as_time = db.Column(db.Boolean(), default=False)
     nonce = db.Column(db.Integer(), default=1)
+
+    def __init__(self, tag, exchange, key, secret, uid, password, timeout, nonceast):
+        self.account_tag = tag
+        self.exchange_id = exchange
+        self.api_key = key
+        self.api_secret = secret
+        self.uid = uid
+        self.password = password
+        self.exchange_timeout = timeout
+        self.nonce_as_time = nonceast
--- mktrade/keystore.py
+++ mktrade/keystore.py
@@ -1,5 +1,8 @@
 from functools import wraps
-from flask import Blueprint, request, render_template, session, url_for, redirect, flash
+from flask import Blueprint, request, render_template, session, url_for, redirect, flash, abort
+from ccxt import exchanges
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from .database import db, Account
 
 
 bp = Blueprint('keystore', __name__, url_prefix='/keystore')
@@ -13,14 +16,12 @@ def auth():
     :return: Response
     """
     if request.method == 'POST':
-        password = request.form.get('pass', None)
-        error = None
+        password = request.form.get('pass')
         if not password or len(password) < 10:
-            error = 'Password length less that 10 symbols'
-        if not error:
+            flash('Password length less that 10 symbols')
+        else:
             session['password'] = password
             return redirect(url_for('keystore.keys'))
-        flash(error)
     return render_template('keystore/auth.html')
 
 
@@ -33,7 +34,95 @@ def password_required(func):
     return wrapper
 
 
-@bp.route('/keys', methods=('GET', 'POST'))
+def keys_process(to_update=None):
+    """
+    Обработчик формы добавления изменения
+
+    :param to_update: Аккаунт для изменения
+    :return: None
+    """
+    tag = request.form.get('tag')
+    exchange = request.form.get('exchange')
+    api_key = request.form.get('api_key')
+    api_secret = request.form.get('api_secret')
+    uid = request.form.get('uid', '')
+    password = request.form.get('pass', '')
+    try:
+        timeout = int(request.form.get('timeout'))
+    except ValueError:
+        timeout = None
+    try:
+        nonce_as_time = bool(request.form.get('nonce_as_time'))
+    except ValueError:
+        nonce_as_time = None
+
+    if not tag:
+        flash('Enter account tag')
+    elif not exchange or exchange not in exchanges:
+        flash('Select exchange')
+    elif not api_key or not api_secret:
+        flash('Enter api key and api secret both')
+    elif timeout is None:
+        flash('Enter correct timeout')
+    elif nonce_as_time is None:
+        flash('Enter correct nonce as time')
+    else:
+        if to_update:
+            to_update.account_tag = tag
+            to_update.exchange_id = exchange
+            to_update.api_key = api_key
+            to_update.api_secret = api_secret
+            to_update.uid = uid
+            to_update.password = password
+            to_update.exchange_timeout = timeout
+            to_update.nonce_as_time = nonce_as_time
+            flash('Account updated')
+        else:
+            db.session.add(Account(tag, exchange, api_key, api_secret, uid, password, timeout, nonce_as_time))
+            flash('Account added')
+        db.session.commit()
+
+
+@bp.route('/', methods=('GET', 'POST'))
 @password_required
 def keys():
-    return 'keys'
+    """
+    Метод вызывается для добавления нового аккаунта
+
+    :return: Response
+    """
+    if request.method == 'POST':
+        keys_process()
+
+    accounts = Account.query.all()
+    return render_template('keystore/keys.html', accounts=accounts, exchanges=exchanges)
+
+
+@bp.route('/<int:key_id>', methods=('GET', 'POST'))
+@password_required
+def keys_by_id(key_id):
+    """
+    Метод вызывается для изменения существующего аккаунта
+
+    :param key_id: ИД существующего аккаунта
+    :return: Response
+    """
+    current_account = None
+    try:
+        current_account = Account.query.filter_by(account_id=key_id).one()
+    except (NoResultFound, MultipleResultsFound):
+        abort(404)
+
+    if request.method == 'POST':
+        act = request.form.get('act')
+        if act == 'add':
+            keys_process(current_account)
+        elif act == 'del':
+            db.session.delete(current_account)
+            db.session.commit()
+            return redirect(url_for('keystore.keys'))
+        else:
+            abort(404)
+
+    accounts = Account.query.all()
+    return render_template('keystore/keys.html', accounts=accounts, current_account=current_account, exchanges=exchanges)
