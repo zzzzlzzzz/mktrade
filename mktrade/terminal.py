fbb50b8090577831d011cdf57c14078d8a70b5c2
zzzzlzzzz@yandex.by <zzzzlzzzz@yandex.by>
2019-03-27 17:43:30 +0400

    Adding AJAX to terminal

--- mktrade/terminal.py
+++ mktrade/terminal.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, abort, session, render_template, request, flash
+from flask import Blueprint, redirect, url_for, abort, session, render_template, request
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from ccxt import BaseError
 from .keystore import password_required
@@ -11,6 +11,11 @@ bp = Blueprint('terminal', __name__)
 @bp.route('/', methods=('GET', ))
 @password_required
 def index():
+    """
+    Переадресует на первый аккаунт, либо на хранилище ключей в случае их отсутствия
+
+    :return: Response
+    """
     first_account = Account.query.first()
     if first_account:
         return redirect(url_for('terminal.index_by_id', account_id=first_account.account_id))
@@ -19,9 +24,16 @@ def index():
 
 
 @bp.route('/<int:account_id>', methods=('GET', ))
-@bp.route('/<int:account_id>/<path:symbol>', methods=('GET', 'POST'))
+@bp.route('/<int:account_id>/<path:symbol>', methods=('GET', ))
 @password_required
 def index_by_id(account_id, symbol=None):
+    """
+    Отображает терминал
+
+    :param account_id: Идентификатор аккаунта
+    :param symbol: Символ для работы
+    :return: Response
+    """
     current_account = None
     try:
         current_account = Account.query.filter_by(account_id=account_id).one()
@@ -35,69 +47,19 @@ def index_by_id(account_id, symbol=None):
     except BaseError:
         abort(503)
 
-    if request.method == 'POST' and symbol and symbol in client.symbols:
-        amount = request.form.get('amount')
-        try:
-            amount = float(amount.replace(',', '.'))
-        except ValueError:
-            amount = None
-        else:
-            amount = client.amount_to_precision(symbol, amount)
-        price = request.form.get('price')
-        try:
-            price = float(price.replace(',', '.'))
-        except ValueError:
-            price = None
-        else:
-            price = client.price_to_precision(symbol, price)
-        mode = request.form.get('mode')
-
-        action = request.form.get('act')
-        if action and action == 'buy':
-            if mode and mode == 'market':
-                if amount:
-                    try:
-                        client.create_market_buy_order(symbol, amount)
-                    except BaseError as e:
-                        flash('Error: {0}'.format(e))
-                else:
-                    flash('Amount invalid')
-            elif mode and mode == 'limit':
-                if amount and price:
-                    try:
-                        client.create_limit_buy_order(symbol, amount, price)
-                    except BaseError as e:
-                        flash('Error: {0}'.format(e))
-                else:
-                    flash('Amount and price invalid')
-            else:
-                flash('Mode invalid')
-        elif action and action == 'sell':
-            if mode and mode == 'market':
-                if amount:
-                    try:
-                        client.create_market_sell_order(symbol, amount)
-                    except BaseError as e:
-                        flash('Error: {0}'.format(e))
-                else:
-                    flash('Amount invalid')
-            elif mode and mode == 'limit':
-                if amount and price:
-                    try:
-                        client.create_limit_sell_order(symbol, amount, price)
-                    except BaseError as e:
-                        flash('Error: {0}'.format(e))
-                else:
-                    flash('Amount and price invalid')
-            else:
-                flash('Mode invalid')
-        else:
-            flash('Action incorrect')
-    return render_template('terminal/index.html', accounts=accounts, current_account=current_account, symbols=client.symbols, current_symbol=symbol, update_interval=current_account.exchange_timeout)
+    return render_template('terminal/index.html', accounts=accounts, current_account=current_account,
+                           symbols=client.symbols, current_symbol=symbol,
+                           update_interval=current_account.exchange_timeout)
 
 
 @bp.route('/balance/<int:account_id>', methods=('GET', ))
 def balance(account_id):
+    """
+    Отображает текущие балансы
+
+    :param account_id: Идентификатор аккаунта
+    :return: Response
+    """
     current_account = None
     try:
         current_account = Account.query.filter_by(account_id=account_id).one()
@@ -116,6 +78,13 @@ def balance(account_id):
 
 @bp.route('/orders/<int:account_id>/<path:symbol>', methods=('GET', ))
 def orders(account_id, symbol):
+    """
+    Отображает открытые ордера
+
+    :param account_id: Идентификатор аккаунта
+    :param symbol: Символ для работы
+    :return: Response
+    """
     current_account = None
     try:
         current_account = Account.query.filter_by(account_id=account_id).one()
@@ -133,17 +102,106 @@ def orders(account_id, symbol):
 
 @bp.route('/cancel/<int:account_id>/<path:symbol>', methods=('POST', ))
 def cancel(account_id, symbol):
+    """
+    Обработчик отмены ордера
+
+    :param account_id: Идентификатор аккаунта
+    :param symbol: Символ для работы
+    :return: Response
+    """
     order_id = request.form.get('order_id')
 
-    current_account = None
     try:
         current_account = Account.query.filter_by(account_id=account_id).one()
     except (NoResultFound, MultipleResultsFound):
-        abort(404)
+        return render_template('terminal/error-msg.html', message='Invalid account')
 
     client = current_account.get_client(session['password'])
     try:
         client.cancel_order(order_id, symbol)
-    except BaseError:
-        abort(503)
-    return redirect(url_for('terminal.index_by_id', account_id=account_id, symbol=symbol))
+    except BaseError as e:
+        return render_template('terminal/error-msg.html', message=e)
+    return render_template('terminal/ok-msg.html', message='Successfully canceled')
+
+
+@bp.route('/create/<int:account_id>/<path:symbol>', methods=('POST', ))
+def create(account_id, symbol):
+    """
+    Обработчик создания ордера
+
+    :param account_id: Идентификатор аккаунта
+    :param symbol: Символ для работы
+    :return: Response
+    """
+    action = request.form.get('act')
+    mode = request.form.get('mode')
+
+    try:
+        current_account = Account.query.filter_by(account_id=account_id).one()
+    except (NoResultFound, MultipleResultsFound):
+        return render_template('terminal/error-msg.html', message='Invalid account')
+
+    client = current_account.get_client(session['password'])
+    try:
+        client.load_markets()
+    except BaseError as e:
+        return render_template('terminal/error-msg.html', message=e)
+
+    if symbol not in client.symbols:
+        return render_template('terminal/error-msg.html', message='Invalid symbol')
+
+    try:
+        amount = float(request.form.get('amount').replace(',', '.'))
+    except ValueError:
+        amount = None
+    else:
+        amount = client.amount_to_precision(symbol, amount)
+
+    try:
+        price = float(request.form.get('price').replace(',', '.'))
+    except ValueError:
+        price = None
+    else:
+        price = client.price_to_precision(symbol, price)
+
+    if action == 'buy':
+        if mode == 'market':
+            if amount:
+                try:
+                    client.create_market_buy_order(symbol, amount)
+                except BaseError as e:
+                    return render_template('terminal/error-msg.html', message=e)
+            else:
+                return render_template('terminal/error-msg.html', message='Invalid amount')
+        elif mode == 'limit':
+            if amount and price:
+                try:
+                    client.create_limit_buy_order(symbol, amount, price)
+                except BaseError as e:
+                    return render_template('terminal/error-msg.html', message=e)
+            else:
+                return render_template('terminal/error-msg.html', message='Invalid amount or price')
+        else:
+            return render_template('terminal/error-msg.html', message='Invalid order type')
+    elif action == 'sell':
+        if mode == 'market':
+            if amount:
+                try:
+                    client.create_market_sell_order(symbol, amount)
+                except BaseError as e:
+                    return render_template('terminal/error-msg.html', message=e)
+            else:
+                return render_template('terminal/error-msg.html', message='Invalid amount')
+        elif mode == 'limit':
+            if amount and price:
+                try:
+                    client.create_limit_sell_order(symbol, amount, price)
+                except BaseError as e:
+                    return render_template('terminal/error-msg.html', message=e)
+            else:
+                return render_template('terminal/error-msg.html', message='Invalid amount or price')
+        else:
+            return render_template('terminal/error-msg.html', message='Invalid order type')
+    else:
+        return render_template('terminat/error-msg.html', message='Action invalid')
+    return render_template('terminal/ok-msg.html', message='Successfully created')
